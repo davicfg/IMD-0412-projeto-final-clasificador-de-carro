fdd8dfcfe9eb42eecb788c250acc5db4d6b9cb14
Davi Carvalho <davicfg@gmail.com>
2023-12-19 21:09:02 -0300

    Criando classe para armazenar carros permitos na categoria black

--- uber/src/main/java/uber/Carro.java
+++ uber/src/main/java/uber/Carro.java
@@ -7,16 +7,17 @@ public class Carro {
 	private int quantidadePortas;
 	private int quantidadeLugares;
 	private Calendar anoFabricacao;
-	private Modelo modelo;
 	private Marca marca;
+	private Modelo modelo;
 
-	public Carro(boolean arCondicionado, int quantidadePortas, int quantidadeLugares, Calendar anoFabricacao,
-			Modelo modelo, Marca marca) {
+	public Carro(boolean arCondicionado, int quantidadePortas, int quantidadeLugares, int anoFabricacao,
+			Marca marca, Modelo modelo) {
 		this.arCondicionado = arCondicionado;
 		this.quantidadePortas = quantidadePortas;
 		this.quantidadeLugares = quantidadeLugares;
-		this.anoFabricacao = anoFabricacao; //TODO https://stackoverflow.com/a/6678850 fazer uma função baseado nessa resposta
-		this.modelo = modelo;
+		this.anoFabricacao = Calendar.getInstance();
+		this.anoFabricacao.set(Calendar.YEAR, anoFabricacao);
 		this.marca = marca;
+		this.modelo = modelo;
 	}
 }
--- uber/src/main/java/uber/ModeloBlack.java
+++ uber/src/main/java/uber/ModeloBlack.java
@@ -3,10 +3,12 @@ package uber;
 import java.util.ArrayList;
 
 public class ModeloBlack {
-	public static final ArrayList<Carro> carros = new ArrayList<Carro>();
-	
+	private ArrayList<Carro> carros = new ArrayList<Carro>();
+	private final static int anoFabricacao = 2016;
+
 	ModeloBlack(){
 		// preecher o arraylist com os carros abaixo
+		//      Marca       Modelo
 		//		Alfa Romeo	Giulietta
 		//		Chevrolet	Bolt
 		//		Chevrolet	Captiva
@@ -20,5 +22,22 @@ public class ModeloBlack {
 		//		Fiat	Freemont
 		//		Ford	Bronco
 		//		Ford	Fusion
+		
+		Carro c = new Carro(true, 4, 5, anoFabricacao, Marca.ALFA_ROMEO, Modelo.GIGIULIETTA);
+		this.carros.add(c);
+		
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.BOLT
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.CAPTIVA
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.FIAT,Modelo.
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.FIAT,Modelo.
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.FORD,Modelo.
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.FORD,Modelo.
+		
 	}
 }
