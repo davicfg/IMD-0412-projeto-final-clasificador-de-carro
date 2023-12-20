6af365f9d38ca9a557dd41e46f0b5c27af6afc84
Davi Carvalho <davicfg@gmail.com>
2023-12-19 21:10:34 -0300

    [WIP]

--- uber/src/main/java/uber/ModeloBlack.java
+++ uber/src/main/java/uber/ModeloBlack.java
@@ -26,18 +26,29 @@ public class ModeloBlack {
 		Carro c = new Carro(true, 4, 5, anoFabricacao, Marca.ALFA_ROMEO, Modelo.GIGIULIETTA);
 		this.carros.add(c);
 		
-		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.BOLT
-		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.CAPTIVA
-		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.
-		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.
-		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.
-		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.
-		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.
-		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.
-		c = new Carro(true, 4, 5, anoFabricacao, Marca.FIAT,Modelo.
-		c = new Carro(true, 4, 5, anoFabricacao, Marca.FIAT,Modelo.
-		c = new Carro(true, 4, 5, anoFabricacao, Marca.FORD,Modelo.
-		c = new Carro(true, 4, 5, anoFabricacao, Marca.FORD,Modelo.
-		
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.BOLT);
+		this.carros.add(c);
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.CAPTIVA);
+		this.carros.add(c);
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.EQUINOX);
+		this.carros.add(c);
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.GRAND_VITARA);
+		this.carros.add(c);
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.MALIBU);
+		this.carros.add(c);
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.CRUZE);
+		this.carros.add(c);
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.TRACKER);
+		this.carros.add(c);
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.CHEVROLET,Modelo.TRAILBLAZER);
+		this.carros.add(c);
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.FIAT,Modelo.FASTBACK);
+		this.carros.add(c);
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.FIAT,Modelo.FREEMONT);
+		this.carros.add(c);
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.FORD,Modelo.BRONCO);
+		this.carros.add(c);
+		c = new Carro(true, 4, 5, anoFabricacao, Marca.FORD,Modelo.FUSION);
+		this.carros.add(c);
 	}
 }
