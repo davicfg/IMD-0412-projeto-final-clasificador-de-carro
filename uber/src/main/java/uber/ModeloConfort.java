cdaa30394c9605d5b7fa3051a348face0efbdd46
Cleiton Lima <acrl.ribeiro17@gmail.com>
2023-12-21 00:35:03 -0300

    Criando escopo para carros Confort

---
+++ uber/src/main/java/uber/ModeloConfort.java
@@ -0,0 +1,14 @@
+package uber;
+
+import java.util.ArrayList;
+
+public class ModeloConfort {
+	
+	private ArrayList<Carro> carros = new ArrayList<Carro>();
+	private final static int anoFabricacao = 2016;
+	
+	ModeloConfort() 
+	{
+		
+	}
+}
